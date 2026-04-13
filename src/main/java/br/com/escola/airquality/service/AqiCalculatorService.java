89f0fd69393eca294c45c0c8c35b52dd6e6eed5d
Leonardo Matroni <leonardomatroni1@gmail.com>
2026-04-13 18:12:07 +0000

    corrigir imports AqiCalculatorService

--- src/main/java/br/com/escola/airquality/service/AqiCalculatorService.java
+++ src/main/java/br/com/escola/airquality/service/AqiCalculatorService.java
@@ -1,18 +1,13 @@
 package br.com.escola.airquality.service;
 
-import br.com.escola.airquality.config.AirQualityProperties;
 import br.com.escola.airquality.domain.SensorReading.AqiCategory;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class AqiCalculatorService {
-
-    private final AirQualityProperties props;
 
     public AqiResult calculate(BigDecimal co2Ppm, BigDecimal pm25Ugm3, BigDecimal pm10Ugm3, BigDecimal tvocMgm3, BigDecimal no2Ppb) {
         int maxAqi = 0;
@@ -27,31 +22,31 @@ public class AqiCalculatorService {
     }
 
     int calculateCo2Aqi(double co2) {
-        return linearInterpolate(co2, new double[]{400, 800, 1000, 1500, 2000, 5000, 10000}, new int[]{0, 50, 100, 150, 200, 300, 500});
+        return linearInterpolate(co2, new double[]{400,800,1000,1500,2000,5000,10000}, new int[]{0,50,100,150,200,300,500});
     }
     int calculatePm25Aqi(double pm25) {
-        return linearInterpolate(pm25, new double[]{0.0, 12.0, 35.4, 55.4, 150.4, 250.4, 500.4}, new int[]{0, 50, 100, 150, 200, 300, 500});
+        return linearInterpolate(pm25, new double[]{0.0,12.0,35.4,55.4,150.4,250.4,500.4}, new int[]{0,50,100,150,200,300,500});
     }
     int calculatePm10Aqi(double pm10) {
-        return linearInterpolate(pm10, new double[]{0, 54, 154, 254, 354, 424, 604}, new int[]{0, 50, 100, 150, 200, 300, 500});
+        return linearInterpolate(pm10, new double[]{0,54,154,254,354,424,604}, new int[]{0,50,100,150,200,300,500});
     }
     int calculateTvocAqi(double tvoc) {
-        return linearInterpolate(tvoc, new double[]{0.0, 0.3, 1.0, 3.0, 10.0, 25.0, 50.0}, new int[]{0, 50, 100, 150, 200, 300, 500});
+        return linearInterpolate(tvoc, new double[]{0.0,0.3,1.0,3.0,10.0,25.0,50.0}, new int[]{0,50,100,150,200,300,500});
     }
     int calculateNo2Aqi(double no2) {
-        return linearInterpolate(no2, new double[]{0, 53, 100, 360, 649, 1249, 2049}, new int[]{0, 50, 100, 150, 200, 300, 500});
+        return linearInterpolate(no2, new double[]{0,53,100,360,649,1249,2049}, new int[]{0,50,100,150,200,300,500});
     }
 
     int linearInterpolate(double concentration, double[] concBp, int[] aqiBp) {
         if (concentration <= concBp[0]) return aqiBp[0];
-        if (concentration >= concBp[concBp.length - 1]) return aqiBp[aqiBp.length - 1];
-        for (int i = 0; i < concBp.length - 1; i++) {
-            if (concentration >= concBp[i] && concentration < concBp[i + 1]) {
-                double ratio = (concentration - concBp[i]) / (concBp[i + 1] - concBp[i]);
-                return (int) Math.round(aqiBp[i] + ratio * (aqiBp[i + 1] - aqiBp[i]));
+        if (concentration >= concBp[concBp.length-1]) return aqiBp[aqiBp.length-1];
+        for (int i = 0; i < concBp.length-1; i++) {
+            if (concentration >= concBp[i] && concentration < concBp[i+1]) {
+                double ratio = (concentration - concBp[i]) / (concBp[i+1] - concBp[i]);
+                return (int) Math.round(aqiBp[i] + ratio * (aqiBp[i+1] - aqiBp[i]));
             }
         }
-        return aqiBp[aqiBp.length - 1];
+        return aqiBp[aqiBp.length-1];
     }
 
     AqiCategory categorize(int aqi) {
